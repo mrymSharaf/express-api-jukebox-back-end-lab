19e6a19ef3f50b619caab9b3c18bd8c6a1ea7134
Mariam Sharaf <maryamshaider25@gmail.com>
2025-08-16 09:21:02 +0300

    add cors to link to frontend

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require('express')
 const mongoose = require('mongoose')
 const morgen = require('morgan')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const trackRoute = require('./routes/trackRoute')
 
 dotenv.config()
-const app = express() 
+const app = express()
 
 mongoose.connect(process.env.MONGOOSE_URI)
 mongoose.connection.on('connected', () => {
     console.log('connected to mongoose')
 })
 
+app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
 app.use(morgen('dev'))
 app.use('/tracks', trackRoute)
